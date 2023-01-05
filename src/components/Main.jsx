60d94ce674900511e593533832f2f750a9b1664d
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-01-05 12:28:47 +0100

    complete UI forheader and  nav

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+function Main() {
+    return (
+        <main>
+            <section className="text-center">
+                <h2>We are creating a dent on Decentralized Internet</h2>
+                <h3>Join Us</h3>
+                <button type="button">Apply for early access</button>
+            </section>
+        </main>
+    )
+}
+export default Main
