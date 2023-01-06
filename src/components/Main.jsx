f7440a70c10fa408edea1a0130ecd07c19b17503
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-01-06 18:46:30 +0100

    Create UI for third section of main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,13 @@ import rightArrow from "../images/arrow-right.png";
 import codeIcon from "../images/html-code-for-icon-7.jpg";
 import course from "../images/icons8-read-online-48.png";
 import block from "../images/icons8-block-64.png";
+import discord from "../images/discord-logo-png-7617.png";
 
 function Main() {
     return (
-        <main>
-            <section className="text-center">
-                <h2 className="font-poppins text-2xl leading-relaxed font-bold  mb-4">
+        <main className="font-poppins">
+            <section className="text-center fon px-4 mt-20">
+                <h2 className="text-2xl leading-relaxed font-bold  mb-4">
                     We are creating a dent on
                     <span className="text-transparent ml-2 bg-clip-text bg-gradient-to-r from-green-400 to-green-800">
                         Decentralized Internet.
@@ -28,27 +29,27 @@ function Main() {
                     Learn, Build and get NFTified
                 </h3>
                 <button
-                    className="mt-8 bg-black shadow-md rounded-xl font-poppins justify-center items-center mr-auto ml-auto font-semibold  w-4/6 h-16 shadow-green-900 text-white text-lg flex"
+                    className="mt-8 bg-black shadow-md rounded-xl justify-center items-center mr-auto ml-auto font-semibold  w-4/6 h-16 shadow-green-900 text-white text-lg flex"
                     type="button"
                 >
                     <span>Get early access</span>
                     <img className="w-6 ml-2" src={rightArrow} alt="right arrow" />
                 </button>
                 <button
-                    className="mt-8 bg-green-100 shadow-lg rounded-xl font-poppins font-semibold  w-4/6 h-16 shadow-green-900 text-black text-lg"
+                    className="mt-8 bg-green-100 shadow-lg rounded-xl font-semibold  w-4/6 h-16 shadow-green-900 text-black text-lg"
                     type="button"
                 >
                     Login
                 </button>
             </section>
-            <section className="mt-52 text-center py-10 bg-gradient-to-b from-slate-50 via-lime-50 to-lime-100">
-                <p className="text-lg font-barlow font-bold text-teal-900 tracking-wider">
+            <section className="mt-52 text-center py-10 bg-gradient-to-b from-transparent via-lime-50 to-green-50">
+                <p className="text-xl font-barlow font-bold text-teal-900 tracking-wider">
                     choose your path
                 </p>
-                <h2 className="text-4xl mt-4 font-bold font-poppins">
+                <h2 className="text-4xl mt-4 font-bold leading-relaxed">
                     What are you looking for ?
                 </h2>
-                <ul className="mt-10 text-gray-500 font-semibold font-poppins ml-6 flex flex-col">
+                <ul className="mt-10 text-gray-500 font-semibold w-11/12 ml-[8%]">
                     <li>
                         <button
                             className="p-2 w-11/12 h-32 rounded-3xl border-gray-500 border-2 mb-8 flex items-center justify-around"
@@ -77,6 +78,32 @@ function Main() {
                         </button>
                     </li>
                 </ul>
+                <button
+                    className="mt-8 bg-black shadow-md rounded-xl justify-center items-center mr-auto ml-auto font-semibold  w-5/6 h-16 shadow-green-900 text-white text-lg flex"
+                    type="button"
+                >
+                    <span>Check out all courses</span>
+                    <img className="w-6 ml-2" src={rightArrow} alt="right arrow" />
+                </button>
+            </section>
+            <section className="text-center py-40 px-4 bg-gradient-to-b from-teal-50 via-green-50 to-transparent">
+                <p className="text-xl font-barlow font-bold text-teal-900 tracking-wider">
+                    community
+                </p>
+                <h2 className="text-4xl mt-4 font-bold tracking-wide leading-relaxed">
+                    Connect with other builders
+                </h2>
+                <p className="text-lg py-4 font-semibold">
+                    Meet thousands of developers and gamers at all different stages of
+                    their web3 journey.
+                </p>
+                <button
+                    className="mt-8 bg-black shadow-md rounded-xl justify-center items-center mr-auto ml-auto font-semibold  w-4/6 h-16 shadow-green-900 text-white text-lg flex"
+                    type="button"
+                >
+                    <span>Join Discord</span>
+                    <img className="w-14 ml-2" src={discord} alt="Discord" />
+                </button>
             </section>
         </main>
     );
